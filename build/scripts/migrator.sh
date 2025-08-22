3906df49526af2acd86442cceff791746e0babec
vizurth <timrsamara@gmail.com>
2025-08-22 15:31:20 +0400

    feat: add migrator and docker logic

---
+++ build/scripts/migrator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source ../../configs.env
+
+# если хочешь брать host из переменных окружения, можно так:
+export MIGRATION_DSN="host=postgres port=$POSTGRES_PORT dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=disable"
+
+sleep 5  # ждем, чтобы postgres поднялся
+
+goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
